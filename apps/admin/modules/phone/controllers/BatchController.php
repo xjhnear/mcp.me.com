ca4e2ed6383f4414c9f7ee2a600868ad3bb8ba21
near <250327864@qq.com>
2017-11-05 23:11:18 +0800

    finish
    Signed-off-by: near <250327864@qq.com>

--- apps/admin/modules/phone/controllers/BatchController.php
+++ apps/admin/modules/phone/controllers/BatchController.php
@@ -94,6 +94,188 @@ class BatchController extends BackendController
 		}
 		$re_batch = PhoneBatch::save($input);
 
+
+		$handle = fopen($target, 'r');
+		$result = self::input_csv($handle); //解析csv
+		$len_result = count($result);
+		if($len_result==0){
+			return json_encode(array('state'=>0,'msg'=>'没有任何数据'));
+		}
+		$i = 0;
+		$sql="INSERT INTO m_phone_numbers (batch_id,phone_number,operator,city,address) VALUES";
+
+		for ($j = 1; $j < $len_result; $j++) { //循环获取各字段值
+			$phone_number = $result[$j][0]; //中文转码
+			$operator = iconv('gb2312', 'utf-8', $result[$j][1]);
+			$city = iconv('gb2312', 'utf-8', $result[$j][2]);
+			$address = iconv('gb2312', 'utf-8', $result[$j][3]);
+			$tmpstr = "'". $re_batch ."','". $phone_number ."','". $operator ."','". $city ."','". $address ."'";
+			$sql .= "(".$tmpstr."),";
+			$i++;
+		}
+		fclose($handle); //关闭指针
+		$sql = substr($sql,0,-1);   //去除最后的逗号
+		DB::insert($sql);
+
+		$data = array();
+		$data['batch_id'] = $re_batch;
+		$data['count'] = $i;
+		$res = PhoneBatch::save($data);
+
+		if($res){
+			return json_encode(array("state"=>1,'msg'=>'批次添加成功'));
+		}else{
+			return json_encode(array('state'=>0,'msg'=>'批次添加失败'));
+		}
+	}
+
+	public function postAjaxDownFile(){
+		ini_set('max_execution_time', '0');
+		ini_set("memory_limit", "1024M");
+		$batch_id = Input::get('batch_id');
+		$pageSize = Input::get('pageSize');
+		if(!$batch_id) return json_encode(array('state'=>0,'msg'=>'数据异常'));
+		$info_batch = PhoneBatch::getInfo($batch_id);
+		if(!$info_batch) return json_encode(array('state'=>0,'msg'=>'批次不存在'));
+		$search = array();
+		$search['batch_id'] = $info_batch['batch_id'];
+		$batch_code = $info_batch['batch_code'];
+		$pageIndex = 1;
+		if ($pageSize > 0) {
+			$info_num_count = PhoneNumbers::getCount($search);
+			$pages = ceil($info_num_count/$pageSize);
+		} else {
+			$pages = 1;
+		}
+		while($pageIndex<=$pages) {
+			$info_num = PhoneNumbers::getList($search,$pageIndex,$pageSize);
+			if ($info_num) {
+				$str = "手机号码,运营商,城市,地址\n";
+				$str = iconv('utf-8','gb2312',$str);
+				foreach($info_num as $index=>$row){
+					$phone_number = iconv('utf-8','gb2312',$row['phone_number']); //中文转码
+					$operator = iconv('utf-8','gb2312',$row['operator']); //中文转码
+					$city = iconv('utf-8','gb2312',$row['city']); //中文转码
+					$address = iconv('utf-8','gb2312',$row['address']);
+					$str .= $phone_number.",".$operator.",".$city.",".$address."\n"; //用引文逗号分开
+				}
+				$filename = $batch_code .'--'. date('Ymd'); //设置文件名
+				self::saveExcelToLocalFile($str,$filename,$pageIndex); //导出
+			}
+			$pageIndex++;
+		}
+		$zipname = $batch_code .'--'. date('Ymd');
+		$zip = new \ZipArchive();
+		if($zip->open(public_path().'/downloads/'.$zipname.'.zip', \ZipArchive::CREATE) === TRUE) {
+			self::addFileToZip(public_path().'/downloads/'.$filename, $zip); //调用方法，对要打包的根目录进行操作，并将ZipArchive的对象传递给方法
+			$zip->close(); //关闭处理的zip文件
+		}
+
+		$data_batch = array();
+		$data_batch['batch_id'] = $batch_id;
+		$data_batch['down_at'] = time();
+		$data_batch['is_new'] = 0;
+		PhoneBatch::save($data_batch);
+
+		$url = '/downloads/'.$zipname.'.zip';
+		return json_encode(array('state'=>1,'url'=>$url));
+	}
+
+	private function saveExcelToLocalFile($data,$filename,$pageIndex=null){
+		$filePath = '/downloads/'.$filename.'/';
+		if(!is_dir(public_path() . $filePath)) {
+			mkdir(public_path() . $filePath,0777,true);
+		}
+
+		if ($pageIndex) {
+			$fp = fopen(public_path() . $filePath . $filename .'--'. $pageIndex.'.csv','a');
+		} else {
+			$fp = fopen(public_path() . $filePath . $filename .'.csv','a');
+		}
+		fwrite($fp, $data);
+		fclose($fp);
+		return $filePath;
+	}
+
+	private function saveCsvToLocalFile($writer,$filename,$pageIndex=null){
+		$filePath = '/downloads/'.$filename.'/';
+		if(!is_dir(public_path() . $filePath)) {
+			mkdir(public_path() . $filePath,0777,true);
+		}
+		if ($pageIndex) {
+			$writer->save(public_path() . $filePath . $filename .'--'. $pageIndex.'.xlsx');
+		} else {
+			$writer->save(public_path() . $filePath . $filename .'.xlsx');
+		}
+		return $filePath;
+	}
+
+	private function addFileToZip($path,&$zip){
+		$handler=opendir($path); //打开当前文件夹由$path指定。
+		$i = 0;
+		while(($filename=readdir($handler))!==false){
+			if($filename != "." && $filename != ".."){//文件夹文件名字为'.'和‘..’，不要对他们进行操作
+				if(is_dir($path."/".$filename)){// 如果读取的某个对象是文件夹，则递归
+					self::addFileToZip($path."/".$filename, $zip);
+				}else{ //将文件加入zip对象
+					$zip->addFile($path."/".$filename,$filename);
+					$i++;
+				}
+			}
+		}
+		@closedir($path);
+	}
+
+	private function input_csv($handle) {
+		$out = array ();
+		$n = 0;
+		while ($data = fgetcsv($handle, 10000)) {
+			$num = count($data);
+			for ($i = 0; $i < $num; $i++) {
+				$out[$n][$i] = $data[$i];
+			}
+			$n++;
+		}
+		return $out;
+	}
+
+	private function export_csv($filename,$data) {
+		header("Content-type:text/csv");
+		header("Content-Disposition:attachment;filename=".$filename);
+		header('Cache-Control:must-revalidate,post-check=0,pre-check=0');
+		header('Expires:0');
+		header('Pragma:public');
+		echo $data;exit;
+	}
+
+	public function postAjaxUploadFileExcel(){
+		set_time_limit(0);
+		ini_set("memory_limit", "1024M");
+		$batch_code = Input::get('batch_code');
+		if(!Input::hasFile('append_file'))
+			return json_encode(array('state'=>0,'msg'=>'文件不存在'));
+		$file = Input::file('append_file');
+		$tmpfile = $file->getRealPath();
+		$filename = $file->getClientOriginalName();
+		$ext = $file->getClientOriginalExtension();
+		if(!in_array($ext,array('xls','xlsx','csv'))) return json_encode(array('state'=>0,'msg'=>'上传文件格式错误'));
+		$server_path = storage_path() . '/tmp/';
+		$newfilename = microtime() . '.' . $ext;
+		$target = $server_path . $newfilename;
+		$file->move($server_path,$newfilename);
+		$input = array();
+		if($batch_code) {
+			$info_exists = PhoneBatch::getInfoByCode($batch_code);
+			if ($info_exists) {
+				return json_encode(array('state'=>0,'msg'=>'批次Code已存在'));
+			} else {
+				$input['batch_code'] = $batch_code;
+			}
+		} else {
+			return json_encode(array('state'=>0,'msg'=>'批次Code不能为空'));
+		}
+		$re_batch = PhoneBatch::save($input);
+
 		require_once base_path() . '/libraries/SpreadsheetReader/php-excel-reader/excel_reader2.php';
 		require_once base_path() . '/libraries/SpreadsheetReader/SpreadsheetReader.php';
 		$Reader = new \SpreadsheetReader($target);
@@ -136,7 +318,7 @@ class BatchController extends BackendController
 		}
 	}
 
-	public function postAjaxDownFile(){
+	public function postAjaxDownFileExcrl(){
 		ini_set('max_execution_time', '0');
 		ini_set("memory_limit", "1024M");
 		$batch_id = Input::get('batch_id');
@@ -212,71 +394,5 @@ class BatchController extends BackendController
 		return json_encode(array('state'=>1,'url'=>$url));
 	}
 
-	private function saveExcelToLocalFile($writer,$filename,$pageIndex=null){
-		$filePath = '/downloads/'.$filename.'/';
-		if(!is_dir(public_path() . $filePath)) {
-			mkdir(public_path() . $filePath,0777,true);
-		}
-		if ($pageIndex) {
-			$writer->save(public_path() . $filePath . $filename .'--'. $pageIndex.'.xlsx');
-		} else {
-			$writer->save(public_path() . $filePath . $filename .'.xlsx');
-		}
-		return $filePath;
-	}
-
-	private function addFileToZip($path,&$zip){
-		$handler=opendir($path); //打开当前文件夹由$path指定。
-		$i = 0;
-		while(($filename=readdir($handler))!==false){
-			if($filename != "." && $filename != ".."){//文件夹文件名字为'.'和‘..’，不要对他们进行操作
-				if(is_dir($path."/".$filename)){// 如果读取的某个对象是文件夹，则递归
-					self::addFileToZip($path."/".$filename, $zip);
-				}else{ //将文件加入zip对象
-					$zip->addFile($path."/".$filename,$filename);
-					$i++;
-				}
-			}
-		}
-		@closedir($path);
-	}
-
-
-
-	public function getTest(){
-//2222
-//		echo date("H:i:s");
-//		$connect_mysql->query('BEGIN');
-//		$params = array('value'=>'50');
-//		for($i=0;$i<2000000;$i++){
-//		$connect_mysql->insert($params);
-//		if($i%100000==0){
-//		$connect_mysql->query('COMMIT');
-//		$connect_mysql->query('BEGIN');
-//		}
-//		}
-//		$connect_mysql->query('COMMIT');
-//		echo date("H:i:s");
-//3333
-//		$sql= "insert into twenty_million (value) values";
-//		for($i=0;$i<2000000;$i++){
-//			$sql.="('50'),";
-//		};
-//		$sql = substr($sql,0,strlen($sql)-1);
-//		$connect_mysql->query($sql);
-//
-
-		$student=DB::select("select * from m_phone_batch");
-		//返回一个二维数组  $student
-		print_r($student);
-		//以节点树的形式输出结果
-		//dd($student);
-		print_r("123");exit;
-
-//		DB::transaction(function () {
-//			DB::table('users')->update(['id' => 1]);
-//			DB::table('posts')->delete();
-//		});
-	}
 
 }
