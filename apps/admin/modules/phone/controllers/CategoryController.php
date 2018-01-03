f594e990bc31acab35f1ed670436e6d00c77085e
xjhnear <xiajiahui@jucaicat.com>
2018-01-03 16:53:36 +0800

    fix
    Signed-off-by: xjhnear <xiajiahui@jucaicat.com>

--- apps/admin/modules/phone/controllers/CategoryController.php
+++ apps/admin/modules/phone/controllers/CategoryController.php
@@ -37,87 +37,43 @@ class CategoryController extends BackendController
 		ini_set("memory_limit", "1024M");
 		$category_id = Input::get('category_id');
 		$downType = Input::get('downType');
-		$pageSize = Input::get('pageSize');
-		$batch_code_down = Input::get('batch_code_down');
 		if(!$category_id) return json_encode(array('state'=>0,'msg'=>'数据异常'));
 		$info_batch = PhoneBatch::getListByCategory($category_id);
 		if(!$info_batch) return json_encode(array('state'=>0,'msg'=>'分类不存在'));
-		$search = array();
-		$pageIndex = 1;
-		$pages = 1;
-		$search['batch_id'] = $info_batch['batch_id'];
-		$info_num_count = PhoneNumbers::getCount($search);
-		if ($downType > 0) {
-			if($batch_code_down) {
-				$info_exists = PhoneBatch::getInfoByCode($batch_code_down);
-				if ($info_exists) {
-					return json_encode(array('state'=>0,'msg'=>'分类Code已存在'));
-				} else {
-					$input['batch_code'] = $batch_code_down;
-					$input['count'] = ($info_num_count>=$pageSize)?$pageSize:$info_num_count;
-				}
-			} else {
-				return json_encode(array('state'=>0,'msg'=>'分类Code不能为空'));
-			}
-			$re_batch = PhoneBatch::save($input);
-
-			$sql="UPDATE m_phone_numbers SET batch_id = ".$re_batch." WHERE batch_id=".$batch_id." ORDER BY num_id DESC LIMIT ".$pageSize;
-			DB::update($sql);
-			if ($info_num_count>$pageSize) {
-				$data = array();
-				$data['batch_id'] = $batch_id;
-				$data['count'] = $info_num_count-$pageSize;
-				$res = PhoneBatch::save($data);
-			} else {
-				PhoneBatch::del($batch_id);
-			}
-			$batch_id = $re_batch;
-			$search['batch_id'] = $re_batch;
-			$batch_code = $batch_code_down;
-
-		} else {
-			$pageSize = $info_num_count;
-			$search['batch_id'] = $info_batch['batch_id'];
-			$batch_code = $info_batch['batch_code'];
+		$category_code =  'C'.time();
+		$info_num_count = 0;
+		$info_num = array();
+		foreach ($info_batch as $item) {
+			$search = array();
+			$pageIndex = 1;
+			$pageSize = 0;
+			$search['batch_id'] = $item['batch_id'];
+			$pageSize = PhoneNumbers::getCount($search);
+			$info_num_count += $pageSize;
+			$info_tmp = PhoneNumbers::getList($search,$pageIndex,$pageSize);
+			$info_num += $info_tmp;
 		}
 
-//		if ($pageSize > 0) {
-//			$info_num_count = PhoneNumbers::getCount($search);
-//			$pages = ceil($info_num_count/$pageSize);
-//		} else {
-//			$pages = 1;
-//		}
-
-		while($pageIndex<=$pages) {
-			$info_num = PhoneNumbers::getList($search,$pageIndex,$pageSize);
-			if ($info_num) {
-				$str = "手机号码,运营商,城市,地址\n";
-				$str = iconv('utf-8','gb2312',$str);
-				foreach($info_num as $index=>$row){
-					$phone_number = iconv('utf-8','gb2312',$row['phone_number']); //中文转码
-					$operator = iconv('utf-8','gb2312',$row['operator']); //中文转码
-					$city = iconv('utf-8','gb2312',$row['city']); //中文转码
-					$address = iconv('utf-8','gb2312',$row['address']);
-					$str .= $phone_number.",".$operator.",".$city.",".$address."\n"; //用引文逗号分开
-				}
-				$filename = $batch_code .'--'. date('Ymd'); //设置文件名
-				self::saveExcelToLocalFile($str,$filename,$pageIndex); //导出
+		if ($info_num) {
+			$str = "手机号码,运营商,城市,地址\n";
+			$str = iconv('utf-8','gb2312',$str);
+			foreach($info_num as $index=>$row){
+				$phone_number = iconv('utf-8','gb2312',$row['phone_number']); //中文转码
+				$operator = iconv('utf-8','gb2312',$row['operator']); //中文转码
+				$city = iconv('utf-8','gb2312',$row['city']); //中文转码
+				$address = iconv('utf-8','gb2312',$row['address']);
+				$str .= $phone_number.",".$operator.",".$city.",".$address."\n"; //用引文逗号分开
 			}
-			$pageIndex++;
+			$filename = $category_code .'--'. date('Ymd'); //设置文件名
+			self::saveExcelToLocalFile($str,$filename,$pageIndex); //导出
 		}
 
-		$zipname = $batch_code .'--'. date('Ymd');
+		$zipname = $category_code .'--'. date('Ymd');
 		$zip = new \ZipArchive();
 		if($zip->open(public_path().'/downloads/'.$zipname.'.zip', \ZipArchive::CREATE) === TRUE) {
 			self::addFileToZip(public_path().'/downloads/'.$filename, $zip); //调用方法，对要打包的根目录进行操作，并将ZipArchive的对象传递给方法
 			$zip->close(); //关闭处理的zip文件
 		}
-
-		$data_batch = array();
-		$data_batch['batch_id'] = $batch_id;
-		$data_batch['down_at'] = time();
-		$data_batch['is_new'] = 0;
-		PhoneBatch::save($data_batch);
 
 		$url = '/downloads/'.$zipname.'.zip';
 		return json_encode(array('state'=>1,'url'=>$url));
@@ -129,8 +85,14 @@ class CategoryController extends BackendController
 		$batch_code = 'B'.time();
 		$category = Input::get('category','');
 		$ids = Input::get('ids');
-		$bids = Input::get('bids');
-
+		$cids = Input::get('bids');
+		$bids = array();
+		foreach ($cids as $cid) {
+			$batch_arr = PhoneBatch::getListByCategory($cid);
+			foreach ($batch_arr as $batch_item) {
+				$bids[] = $batch_item['batch_id'];
+			}
+		}
 		$input = array();
 		$i_c = $unicom_c = $mobile_c = $telecom_c = 0;
 		if($category) {
